28c1d7f05f7221710438278745029d6b1843bfe7
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-08-02 17:23:59 -0300

    Creating chat groups screen

--- src/pages/ChatRoom/index.js
+++ src/pages/ChatRoom/index.js
@@ -1,24 +1,72 @@
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
 import React from 'react';
 
 import auth from '@react-native-firebase/auth';
 import {useNavigation} from '@react-navigation/native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function ChatRoom() {
   const navigation = useNavigation();
 
+  function handleSignOut() {
+    auth()
+      .signOut()
+      .then(() => {
+        navigation.navigate('SignIn');
+      })
+      .catch(() => {
+        console.log('NÃO POSSUI NENHUM USUÁRIO');
+      });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Tela ChatRoom</Text>
-      <Button title="Login" onPress={() => navigation.navigate('SignIn')} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerRoom}>
+        <View style={styles.headerRoomLeft}>
+          <TouchableOpacity onPress={handleSignOut}>
+            <MaterialIcons name="arrow-back" size={28} color="#FFF" />
+          </TouchableOpacity>
+          <Text style={styles.title}>Grupos</Text>
+        </View>
+
+        <TouchableOpacity>
+          <MaterialIcons name="search" size={28} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  headerRoom: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 40,
+    paddingBottom: 20,
+    paddingHorizontal: 10,
+    backgroundColor: '#2E54D4',
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+  },
+  headerRoomLeft: {
+    flexDirection: 'row',
     alignItems: 'center',
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#FFF',
+    paddingLeft: 10,
   },
 });
