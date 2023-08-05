97d5a14221a09a884823a40b3129cbf01227d202
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-08-05 09:56:44 -0300

    Creating search screen

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,18 +1,62 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  TextInput,
+} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function Search() {
+  const [input, setInput] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Tela Procurar</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerInput}>
+        <TextInput
+          placeholder="Digite o nome da sala"
+          value={input}
+          onChangeText={text => setInput(text)}
+          style={styles.input}
+          autoCapitalize={'none'}
+        />
+
+        <TouchableOpacity style={styles.buttonSearch}>
+          <MaterialIcons name="search" size={30} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFF',
+  },
+  containerInput: {
+    flexDirection: 'row',
     justifyContent: 'center',
+    width: '100%',
+    marginVertical: 14,
+  },
+  input: {
+    backgroundColor: '#EBEBEB',
+    marginLeft: 10,
+    height: 50,
+    width: '80%',
+    borderRadius: 4,
+    padding: 5,
+  },
+  buttonSearch: {
+    backgroundColor: '#2E54D4',
+    borderRadius: 4,
     alignItems: 'center',
+    justifyContent: 'center',
+    width: '15%',
+    marginLeft: 5,
+    marginRight: 10,
   },
 });
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignIn from '../pages/SignIn';
 import ChatRoom from '../pages/ChatRoom';
 import Messages from '../pages/Messages';
+import Search from '../pages/Search';
 
 const AppStack = createNativeStackNavigator();
 
@@ -32,6 +33,12 @@ function AppRoutes() {
         options={({route}) => ({
           title: route.params.thread.name,
         })}
+      />
+
+      <AppStack.Screen
+        name="Search"
+        component={Search}
+        options={{title: 'Procurando algum grupo?'}}
       />
     </AppStack.Navigator>
   );
